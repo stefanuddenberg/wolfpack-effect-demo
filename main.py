18d9d4e9bbfb7684455a69ed45ae04bb2ad44a76
Stefan Uddenberg <stefan.uddenberg@gmail.com>
2025-03-25 23:19:19 -0500

    Make helpful getters and setters

--- main.py
+++ main.py
@@ -29,12 +29,12 @@ class Wolf:
     def __init__(
         self,
         window,
+        pos=None,
         speed=config["wolf"]["speed"],
         color=config["wolf"]["color"],
         size=config["wolf"]["size"],
         vertices=config["wolf"]["vertices"],
         direction_noise=config["wolf"]["direction_noise"],
-        pos=None,
     ):
         if pos is None:
             pos = [
@@ -58,8 +58,8 @@ class Wolf:
     def calculate_facing_angle(self, target_pos, units=config["display"]["units"]):
         # Calculate angle to the target in radians
         # 0° is horizontal, 90° is vertical (counter-clockwise)
-        dx = target_pos[0] - self.stimulus.pos[0]
-        dy = target_pos[1] - self.stimulus.pos[1]
+        dx = target_pos[0] - self.pos[0]
+        dy = target_pos[1] - self.pos[1]
         angle = np.degrees(np.arctan2(dy, dx))
 
         # Convert to PsychoPy's clockwise orientation system
@@ -67,19 +67,11 @@ class Wolf:
         return angle if units == "deg" else np.radians(angle)
 
     def update(self, target_pos, face_sheep):
-        new_pos = self.stimulus.pos + np.array(
+        new_pos = self.pos + np.array(
             [np.cos(self.direction) * self.speed, np.sin(self.direction) * self.speed]
         )
 
-        if abs(new_pos[0]) > HORIZONTAL_BOUNDARY:
-            self.direction = np.pi - self.direction
-            new_pos[0] = np.clip(new_pos[0], -HORIZONTAL_BOUNDARY, HORIZONTAL_BOUNDARY)
-
-        if abs(new_pos[1]) > VERTICAL_BOUNDARY:
-            self.direction = -self.direction
-            new_pos[1] = np.clip(new_pos[1], -VERTICAL_BOUNDARY, VERTICAL_BOUNDARY)
-
-        self.stimulus.pos = new_pos
+        self.pos = new_pos  # automatically keeps within bounds!
 
         # Gradually adjust direction with some randomness
         self.direction += np.random.normal(0, self.direction_noise)
@@ -87,12 +79,44 @@ class Wolf:
         angle_to_sheep_deg = self.calculate_facing_angle(
             target_pos, units=config["display"]["units"]
         )
-        self.stimulus.ori = (
-            angle_to_sheep_deg if face_sheep else angle_to_sheep_deg + 90
-        )
+        self.ori = angle_to_sheep_deg if face_sheep else angle_to_sheep_deg + 90
 
     def draw(self):
         self.stimulus.draw()
+
+    # We use properties for convenience
+    @property
+    def pos(self):
+        return self.stimulus.pos
+
+    @pos.setter
+    def pos(self, new_pos):
+        """Sets the position of the wolf, keeping it within bounds.
+
+        Args:
+            new_pos (tuple): The new position of the wolf
+        """
+
+        # Check if we hit a boundary and need to bounce off in the opposite direction
+        if abs(new_pos[0]) > HORIZONTAL_BOUNDARY:
+            self.direction = np.pi - self.direction
+
+        if abs(new_pos[1]) > VERTICAL_BOUNDARY:
+            self.direction = -self.direction
+
+        # Keep in bounds
+        bounded_x = np.clip(new_pos[0], -HORIZONTAL_BOUNDARY, HORIZONTAL_BOUNDARY)
+        bounded_y = np.clip(new_pos[1], -VERTICAL_BOUNDARY, VERTICAL_BOUNDARY)
+
+        self.stimulus.pos = (bounded_x, bounded_y)
+
+    @property
+    def ori(self):
+        return self.stimulus.ori
+
+    @ori.setter
+    def ori(self, new_ori):
+        self.stimulus.ori = new_ori
 
 
 class Sheep:
@@ -105,10 +129,23 @@ class Sheep:
             pos=CENTER,
         )
         self.mouse = event.Mouse(win=window)
+        self.last_mouse_x, self.last_mouse_y = self.mouse.getPos()
 
     def update(self):
-        self.position = self.mouse.getPos()
-        self.stimulus.pos = self.position
+        current_mouse_x, current_mouse_y = self.mouse.getPos()
+
+        # Get mouse movement since last frame
+        delta_x = current_mouse_x - self.last_mouse_x
+        delta_y = current_mouse_y - self.last_mouse_y
+
+        # Update sheep position based on movement since last frame
+        new_x = self.pos[0] + delta_x
+        new_y = self.pos[1] + delta_y
+
+        self.pos = (new_x, new_y)  # automatically keeps within bounds!
+
+        # Update last mouse position
+        self.last_mouse_x, self.last_mouse_y = current_mouse_x, current_mouse_y
 
     def draw(self):
         self.stimulus.draw()
@@ -117,19 +154,31 @@ class Sheep:
     def pos(self):
         return self.stimulus.pos
 
+    @pos.setter
+    def pos(self, new_pos):
+        """Sets the position of the sheep within the boundaries defined.
+
+        Args:
+            new_pos (tuple): The new position of the sheep
+        """
+        bounded_x = np.clip(new_pos[0], -HORIZONTAL_BOUNDARY, HORIZONTAL_BOUNDARY)
+        bounded_y = np.clip(new_pos[1], -VERTICAL_BOUNDARY, VERTICAL_BOUNDARY)
+
+        self.stimulus.pos = (bounded_x, bounded_y)
+
 
 def main(face_sheep=True):
     win = visual.Window(
+        monitor=mon,
         size=WINDOW_SIZE,
         color=config["display"]["bg_color"],
         units=config["display"]["units"],
         allowGUI=config["display"]["allow_gui"],
-        monitor=mon,
         screen=config["display"]["screen"],
         fullscr=config["display"]["full_screen"],
     )
 
-    # have to set mouse visibility after creating the window
+    # Need to set mouse visibility after creating the window
     # __init__ doesn't take it as an argument
     win.mouseVisible = config["display"]["mouse_visible"]
 
